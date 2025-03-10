184d0aa73442479fe43d7c0cbc80f38c7799138a
pachuco <pachuco>
2025-03-10 16:03:11 -0400

    ajouter objet game dans GameViewModel

--- src/app/src/main/java/com/example/c61_shogi_rag/ui/screens/game_screen/GameViewModel.kt
+++ src/app/src/main/java/com/example/c61_shogi_rag/ui/screens/game_screen/GameViewModel.kt
@@ -1,8 +1,16 @@
 package com.example.c61_shogi_rag.ui.screens.game_screen
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import com.example.c61_shogi_rag.engine.game.Board
+import com.example.c61_shogi_rag.engine.game.Game
 
 class GameViewModel: ViewModel() {
-    var board = Board()
+    private var game: Game = Game(true)
+
+    init {
+        game.GameInit()
+    }
+
 }
