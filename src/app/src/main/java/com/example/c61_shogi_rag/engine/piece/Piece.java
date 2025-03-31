6a63a17d0a5b0254b5ef0b5b26d1c4b21f6280b8
Veilleux Gabriel <e.gveilleux@etu.cvm.qc.ca>
2025-03-31 11:26:48 -0400

    regle des coord

--- src/app/src/main/java/com/example/c61_shogi_rag/engine/piece/Piece.java
+++ src/app/src/main/java/com/example/c61_shogi_rag/engine/piece/Piece.java
@@ -127,7 +127,7 @@ public class Piece {
                     tempY = direction[j];
                 }
             }
-            if (deltaY == tempX && deltaX == tempY) {
+            if (deltaY == tempY && deltaX == tempX) {
                 return true;
             }
         }
