4317bbf12860a03e80e1eebcd8807a951c42b53a
Gabriel <115233473+Gab4740@users.noreply.github.com>
2025-04-01 17:25:53 -0400

    Small Patches

--- src/app/src/main/java/com/example/c61_shogi_rag/engine/piece/ShogiPiece.java
+++ src/app/src/main/java/com/example/c61_shogi_rag/engine/piece/ShogiPiece.java
@@ -3,7 +3,7 @@ package com.example.c61_shogi_rag.engine.piece;
 import com.example.c61_shogi_rag.engine.game.Board;
 
 public abstract class ShogiPiece {
-    private final byte ID;  // ID neg
+    private final byte ID;  // ID negatif = Noir, ID positif = Blanc
     private final byte ID_PROMU;
     private final int IMAGE_ID;
     private final int IMAGE_ID_PROMU;
@@ -18,23 +18,8 @@ public abstract class ShogiPiece {
         NOM = nom;
         DIRECTIONS = directions;
     }
+
     public abstract boolean isValidMove(Move move, Board board);
-    protected boolean checkSteps(int deltaX, int deltaY){
-        int tempX = 0, tempY = 0;
-        for (int[] direction : DIRECTIONS) {
-            for (int j = 0; j < direction.length; j++) {
-                if (j == 0) {
-                    tempX = direction[j];
-                } else if (j == 1) {
-                    tempY = direction[j];
-                }
-            }
-            if (deltaY == tempX && deltaX == tempY) {
-                return true;
-            }
-        }
-        return false;
-    }
 
     public byte getID() {
         return ID;
