1fcac3606da1c9fa41c4e7a93644f831bdb3e4f2
Veilleux Gabriel <e.gveilleux@etu.cvm.qc.ca>
2025-04-02 11:37:19 -0400

    add undo, redo move

---
+++ src/app/src/main/java/com/example/c61_shogi_rag/engine/game/MoveManager.java
@@ -0,0 +1,39 @@
+package com.example.c61_shogi_rag.engine.game;
+
+import com.example.c61_shogi_rag.engine.piece.Move;
+import com.example.c61_shogi_rag.engine.piece.Position;
+
+public class MoveManager {
+    private final byte originalPiece;
+    private final byte originalTarget;
+    private final Move move;
+
+    public MoveManager(byte originalPiece, byte originalTarget, Move move) {
+        this.originalPiece = originalPiece;
+        this.originalTarget = originalTarget;
+        this.move = move;
+    }
+
+    /**
+     * Modifie l'instance du board pour accomplir le move.
+     *
+     * @param board -> Board a modifier
+     * */
+    public void do_move_on_board(Board board){
+        Position posOriginal = new Position(move.getCurrentPosition().getPosX(), move.getCurrentPosition().getPosY());
+        Position posNew = new Position(move.getNextPosition().getPosX(), move.getNextPosition().getPosY());
+        board.movePieceTo(posOriginal, posNew);
+    }
+
+    /**
+     * Modifie l'instance du board pour undo un move déjà accompli.
+     *
+     * @param board -> Board a modifier
+     * */
+    public void undo_move_on_board(Board board){
+        Position posOriginal = new Position(move.getCurrentPosition().getPosX(), move.getCurrentPosition().getPosY());
+        Position posNew = new Position(move.getNextPosition().getPosX(), move.getNextPosition().getPosY());
+        board.movePieceTo(posNew, posOriginal);
+        board.setPieceAt(originalTarget, posNew);
+    }
+}
--- src/app/src/main/java/com/example/c61_shogi_rag/engine/minimax/MoveGeneration.java
+++ src/app/src/main/java/com/example/c61_shogi_rag/engine/minimax/MoveGeneration.java
@@ -1,4 +1,5 @@
 package com.example.c61_shogi_rag.engine.minimax;
 
 public class MoveGeneration {
+
 }
