cb8ae09def370fa41bd6f5d345ea6b6ed7eab250
Crager <crager92@gmail.com>
2025-04-19 19:54:36 -0400

    optimisation des methode d'evaluation et debut de Control square

--- src/app/src/main/java/com/example/c61_shogi_rag/engine/minimax/Evaluation.java
+++ src/app/src/main/java/com/example/c61_shogi_rag/engine/minimax/Evaluation.java
@@ -2,6 +2,7 @@ package com.example.c61_shogi_rag.engine.minimax;
 
 import com.example.c61_shogi_rag.engine.game.Board;
 import com.example.c61_shogi_rag.engine.game.PromotionState;
+import com.example.c61_shogi_rag.engine.piece.Move;
 import com.example.c61_shogi_rag.engine.piece.PieceIDs;
 import com.example.c61_shogi_rag.engine.piece.Position;
 import com.example.c61_shogi_rag.engine.piece.ShogiPiece;
@@ -18,7 +19,13 @@ public class Evaluation {
     private static final int DISTANCE_TO_KING_ADJUST = 120;
     private static final int CONTROL_CENTER_ADJUST = 80;
 
-    private static final int[][] POSITION_NEXT_TO_KING = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+    private static final int[][] POSITION_ADJACENTE = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+
+    private static final int[][] FOU_DIRECTION = {{-1,-1},{-1,1},{1,-1},{1,1}};
+
+    private static final int[][] CHAR_DIRECTION = {{0,-1},{0,1},{-1,0},{1,0}};
+
+
 
     public static int material_eval(Board board, PromotionState promotionStateMap, Hashtable<Byte, ShogiPiece> piece) {
         int totalScore = 0;
@@ -31,7 +38,7 @@ public class Evaluation {
         return totalScore * MATERIAL_EVAL_ADJUST;
     }
 
-    //========================================================================================================
+    /** ======================================================================================================== **/
 
     public int controleCenter(Board board){
 
@@ -55,7 +62,7 @@ public class Evaluation {
 
     }
 
-    //========================================================================================================
+    /** ======================================================================================================== **/
 
     //evaluation du castling
     public static int evaluateCastling(Board board){
@@ -92,7 +99,7 @@ public class Evaluation {
         //int[][] pieceACoter = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
 
         //verification de la position des pieces a cote du roi
-        for (int[] pos : POSITION_NEXT_TO_KING){
+        for (int[] pos : POSITION_ADJACENTE){
             int x = kx + pos[0];
             int y = ky + pos[1];
 
@@ -138,7 +145,7 @@ public class Evaluation {
         //int[][] pieceACoter = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
 
         //verification si y'a des piece autour du roi
-        for (int[] pos : POSITION_NEXT_TO_KING){
+        for (int[] pos : POSITION_ADJACENTE){
             int x = kx + pos[0];
             int y = ky + pos[1];
 
@@ -184,7 +191,7 @@ public class Evaluation {
         return null;
     }
 
-    //========================================================================================================
+    /** ======================================================================================================== **/
 
     //evaluation de la safety du roi
     public static int evaluateKingSafety(Board board){
@@ -214,7 +221,7 @@ public class Evaluation {
         int nbrEscapeRoute = 0;
 
         // Parcourir les positions adjacentes
-        for (int[] pos : POSITION_NEXT_TO_KING){
+        for (int[] pos : POSITION_ADJACENTE){
             int x = kx + pos[0];
             int y = ky + pos[1];
 
@@ -282,7 +289,7 @@ public class Evaluation {
 
         int nbrPieceAutour = 0;
 
-        for (int[] pos : POSITION_NEXT_TO_KING){
+        for (int[] pos : POSITION_ADJACENTE){
             int x = kx + pos[0];
             int y = ky + pos[1];
 
@@ -377,7 +384,7 @@ public class Evaluation {
         return score;
     }
 
-    //========================================================================================================
+    /** ======================================================================================================== **/
 
 
     public static int evaluateDistanceToTheKing(Board board, Hashtable<Byte, ShogiPiece> piece){
@@ -438,7 +445,72 @@ public class Evaluation {
     }
 
 
+    /** ======================================================================================================== **/
 
+    /** CODE A OPTIMISER SA MERE **/
+    public static int evaluateControlleSquares(Board board, Hashtable<Byte, ShogiPiece> piece){
+        int score = 0;
+        int boardSize = board.getBOARD_SIZE();
+
+        for(int i = 0; i < boardSize; i++){
+            for(int j = 0; j < boardSize; j++) {
+                Position pos = new Position(i, j);
+
+                int iaControl = countControllingPieces(board, pos, false, piece);
+                int joueurControl = countControllingPieces(board, pos, true, piece);
+
+                int totalControl = iaControl - joueurControl;
+
+                if (i < 3) {
+                    if (totalControl > 0){
+                        score += totalControl * 3;
+                    }
+                }
+                else if (i > 5)
+                {
+                    if (totalControl < 0)
+                    {
+                        score += totalControl * 3;
+                    }
+                    else if (totalControl > 0)
+                    {
+                        score += totalControl;
+                    }
+                }
+                else{
+                    score += totalControl;
+                }
+            }
+        }
+
+        return score;
+    }
+
+
+    private static int countControllingPieces(Board board, Position newPos, boolean color, Hashtable<Byte, ShogiPiece> pieces){
+        int count = 0;
+
+        Vector<Position> piecesPos = board.getPositionFromColor(color);
+
+        for (Position pos : piecesPos){
+            if (pos.getPosX() == newPos.getPosX() && pos.getPosY() == newPos.getPosY()){
+                continue;
+            }
+
+            byte pieceValue = board.getPieceAt(pos);
+            ShogiPiece shogiPiece = pieces.get(pieceValue);
+
+            if (shogiPiece != null){
+                Move move = new Move(pos, newPos);
+
+                if (shogiPiece.isValidMove(move, board)){
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
 
 
     //TODO rajouter les autres methodes d'evaluations
--- src/app/src/main/java/com/example/c61_shogi_rag/engine/minimax/difficulties/Hard.java
+++ src/app/src/main/java/com/example/c61_shogi_rag/engine/minimax/difficulties/Hard.java
@@ -20,6 +20,8 @@ public class Hard extends EvaluationStrategies {
         score += Evaluation.evaluateKingSafety(board);
         score += Evaluation.evaluateDistanceToTheKing(board, pieces);
         score += Evaluation.material_eval(board, promotions, pieces);
+         /** NE PAS DECOMMENTER POUR L'INSTANT A PART SI TU VEUT QUE L'IA NE JOUE JAMAIS **/
+        //score += Evaluation.evaluateControlleSquares(board, pieces);
 
         return score;
     }
